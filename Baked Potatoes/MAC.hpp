6d5e27d399466c29a99b4053d7b55e7811338c27
KimJongUn42 <antroseconomides@hotmail.com>
2015-01-07 14:35:07 +0200

    Fix an HMAC bug when using Keys larger than the Hash Function's Block Size

--- Baked Potatoes/MAC.hpp
+++ Baked Potatoes/MAC.hpp
@@ -24,7 +24,7 @@ namespace bpl
 
 					if (Key.length() > Hash::BlockSize)
 						Key = ByteToByteString(Function.Hash(Key));
-					else if (Key.length() < Hash::BlockSize)
+					if (Key.length() < Hash::BlockSize)
 						Key.resize(Hash::BlockSize, 0x00);
 
 					std::string InnerKeyPad(64, 0x00);
@@ -34,7 +34,7 @@ namespace bpl
 					std::transform(Key.begin(), Key.end(), OutterKeyPad.begin(), [](std::string::value_type Byte) { return Byte ^ 0x5c; });
 
 					std::string InnerString = InnerKeyPad + Message;
-					auto InnerHash = Function.Hash(InnerString); // Uh oh
+					auto InnerHash = Function.Hash(InnerString);
 					std::string InnerResult = ByteToByteString(InnerHash);
 					return Function.Hash(OutterKeyPad + InnerResult);
 				}
