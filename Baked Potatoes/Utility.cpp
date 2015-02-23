34ddfa1f2e5e7b0a5c2f3f9fbcc07abdd18031cf
KimJongUn42 <antroseconomides@hotmail.com>
2015-02-23 12:26:03 +0200

    Utility: Fix compiler Warnings

--- Baked Potatoes/Utility.cpp
+++ Baked Potatoes/Utility.cpp
@@ -14,36 +14,48 @@ namespace bpl
 		{
 			if (Endianness())
 				return _byteswap_uint64(Value);
+			else
+				return Value;
 		}
 
 		uint32_t BigEndian32(uint32_t Value)
 		{
 			if (Endianness())
 				return _byteswap_ulong(Value);
+			else
+				return Value;
 		}
 
 		uint16_t BigEndian16(uint16_t Value)
 		{
 			if (Endianness())
 				return _byteswap_ushort(Value);
+			else
+				return Value;
 		}
 
 		uint64_t LittleEndian64(uint64_t Value)
 		{
 			if (!Endianness())
 				return _byteswap_uint64(Value);
+			else
+				return Value;
 		}
 
 		uint32_t LittleEndian32(uint32_t Value)
 		{
 			if (!Endianness())
 				return _byteswap_ulong(Value);
+			else
+				return Value;
 		}
 
 		uint16_t LittleEndian16(uint16_t Value)
 		{
 			if (!Endianness())
 				return _byteswap_ushort(Value);
+			else
+				return Value;
 		}
 	}
 }
