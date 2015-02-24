fd0d722758256fe13aaab91bfda399661d8d6396
KimJongUn42 <antroseconomides@hotmail.com>
2015-02-24 11:35:28 +0200

    Utility: Condense Endianness conversions

--- Baked Potatoes/Utility.cpp
+++ Baked Potatoes/Utility.cpp
@@ -12,50 +12,32 @@ namespace bpl
 
 		uint64_t BigEndian64(uint64_t Value)
 		{
-			if (Endianness())
-				return _byteswap_uint64(Value);
-			else
-				return Value;
+			return Endianness() ? _byteswap_uint64(Value) : Value;
 		}
 
 		uint32_t BigEndian32(uint32_t Value)
 		{
-			if (Endianness())
-				return _byteswap_ulong(Value);
-			else
-				return Value;
+			return Endianness() ? _byteswap_ulong(Value) : Value;
 		}
 
 		uint16_t BigEndian16(uint16_t Value)
 		{
-			if (Endianness())
-				return _byteswap_ushort(Value);
-			else
-				return Value;
+			return Endianness() ? _byteswap_ushort(Value) : Value;
 		}
 
 		uint64_t LittleEndian64(uint64_t Value)
 		{
-			if (!Endianness())
-				return _byteswap_uint64(Value);
-			else
-				return Value;
+			return !Endianness() ? _byteswap_uint64(Value) : Value;
 		}
 
 		uint32_t LittleEndian32(uint32_t Value)
 		{
-			if (!Endianness())
-				return _byteswap_ulong(Value);
-			else
-				return Value;
+			return !Endianness() ? _byteswap_ulong(Value) : Value;
 		}
 
 		uint16_t LittleEndian16(uint16_t Value)
 		{
-			if (!Endianness())
-				return _byteswap_ushort(Value);
-			else
-				return Value;
+			return !Endianness() ? _byteswap_ushort(Value) : Value;
 		}
 	}
 }
