e8c2c30aa8c737578bd7d086f2927f9438d5ee75
KimJongUn42 <antroseconomides@hotmail.com>
2015-03-10 19:59:14 +0200

    Pixel: Now POD with no accessor functions

--- Baked Potatoes/Pixel.hpp
+++ Baked Potatoes/Pixel.hpp
@@ -1,57 +1,27 @@
-//#pragma once
-//
-//#include <cstdint>
-//
-//namespace bpl
-//{
-//	namespace image
-//	{
-//		class Pixel24
-//		{
-//		public:
-//			using component_type = std::uint8_t;
-//
-//			Pixel24();
-//
-//			Pixel24(component_type R, component_type G, component_type B);
-//
-//			operator Pixel32() const;
-//
-//			component_type& R();
-//
-//			const component_type R() const;
-//
-//			component_type& G();
-//
-//			component_type const G() const;
-//
-//			component_type& B();
-//
-//			const component_type B() const;
-//
-//		protected:
-//			component_type Red;
-//			component_type Green;
-//			component_type Blue;
-//		};
-//
-//		class Pixel32 : public Pixel24
-//		{
-//		public:
-//			using component_type = Pixel24::component_type;
-//
-//			Pixel32();
-//
-//			Pixel32(component_type R, component_type G, component_type B, component_type A);
-//
-//			operator Pixel24();
-//
-//			component_type& A();
-//
-//			component_type const A() const;
-//
-//		private:
-//			component_type Alpha;
-//		};
-//	}
-//}
+#pragma once
+
+#include <cstdint>
+
+namespace bpl
+{
+	namespace image
+	{
+		struct Pixel24
+		{
+			using component_type = std::uint8_t;
+			static const std::size_t Depth = 24;
+
+			component_type Red;
+			component_type Green;
+			component_type Blue;
+		};
+
+		struct Pixel32 : public Pixel24
+		{
+			using component_type = Pixel24::component_type;
+			static const std::size_t Depth = 32;
+
+			component_type Alpha;
+		};
+	}
+}
