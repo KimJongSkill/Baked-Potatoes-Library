8a23a3ff8fa6183dd85895f57e5dd1f6c6515287
KimJongSkill <antroseconomides@hotmail.com>
2016-05-08 19:24:53 +0300

    Crypt: Correct style

--- Baked Potatoes/Crypt.cpp
+++ Baked Potatoes/Crypt.cpp
@@ -105,8 +105,10 @@ namespace bpl
 		template <class Type>
 		bool IvyRNG::Generate(Type& Argument, std::function<int(Type*)> RDRAND)
 		{
-			// In case RDRAND fails to return a random number,
-			// it is recommended to retry 10 times
+			/*
+			*	In case RDRAND fails to return a random number,
+			*	it is recommended to retry 10 times.
+			*/
 			for (std::size_t i = 0; i < 10; ++i)
 			{
 				int Result = RDRAND(&Argument);
@@ -147,10 +149,10 @@ namespace bpl
 		void AES<KeySize>::MixColumns()
 		{
 			/*
-			This is sort of like a matrix multiplication, but with XORs
-			When we have a 0x1 in the matrix, we simply XOR the byte
-			When we have a 0x2 we perform the xtime operation and then XOR
-			When we have a 0x3 we do both, since 0x1 & 0x2 = 0x3
+			*	This is sort of like a matrix multiplication, but with XORs.
+			*	- when we have a 0x1 in the matrix, we simply XOR the byte
+			*	- when we have a 0x2 we perform the xtime operation and then XOR
+			*	- when we have a 0x3 we do both, since 0x1 & 0x2 = 0x3
 			*/
 
 			std::array<std::uint8_t, 4> NewColumn;
@@ -166,21 +168,22 @@ namespace bpl
 				unsigned char b[4];
 				unsigned char c;
 				unsigned char h;
-				/* The array 'a' is simply a copy of the input array 'r'
-				* The array 'b' is each element of the array 'a' multiplied by 2
-				* in Rijndael's Galois field
-				* a[n] ^ b[n] is element n multiplied by 3 in Rijndael's Galois field */
-				for (c = 0;c<4;c++) {
+				/* 
+				*	The array 'a' is simply a copy of the input array 'r'.
+				*	The array 'b' is each element of the array 'a' multiplied by 2 in Rijndael's Galois field.
+				*	a[n] ^ b[n] is element n multiplied by 3 in Rijndael's Galois field.
+				*/
+				for (c = 0; c < 4; c++) {
 					a[c] = State[i + c];
-					/* h is 0xff if the high bit of r[c] is set, 0 otherwise */
-					h = (unsigned char)((signed char)a[c] >> 7); /* arithmetic right shift, thus shifting in either zeros or ones */
-					b[c] = a[c] << 1; /* implicitly removes high bit because b[c] is an 8-bit char, so we xor by 0x1b and not 0x11b in the next line */
-					b[c] ^= 0x1B & h; /* Rijndael's Galois field */
+					// h is 0xff if the high bit of r[c] is set, 0 otherwise
+					h = (unsigned char)((signed char)a[c] >> 7); // arithmetic right shift, thus shifting in either zeros or ones
+					b[c] = a[c] << 1; // implicitly removes high bit because b[c] is an 8-bit char, so we xor by 0x1b and not 0x11b in the next line
+					b[c] ^= 0x1B & h; // Rijndael's Galois field
 				}
-				NewColumn[0] = b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1]; /* 2 * a0 + a3 + a2 + 3 * a1 */
-				NewColumn[1] = b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2]; /* 2 * a1 + a0 + a3 + 3 * a2 */
-				NewColumn[2] = b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3]; /* 2 * a2 + a1 + a0 + 3 * a3 */
-				NewColumn[3] = b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]; /* 2 * a3 + a2 + a1 + 3 * a0 */
+				NewColumn[0] = b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1]; // 2 * a0 + a3 + a2 + 3 * a1
+				NewColumn[1] = b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2]; // 2 * a1 + a0 + a3 + 3 * a2
+				NewColumn[2] = b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3]; // 2 * a2 + a1 + a0 + 3 * a3
+				NewColumn[3] = b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]; // 2 * a3 + a2 + a1 + 3 * a0
 
 				std::copy(NewColumn.cbegin(), NewColumn.cend(), State.begin() + i);
 			} 
